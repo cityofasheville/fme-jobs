7fd95f334bd3a476747b5a7060f7ba9f3830739b
Dave Michelson <daveism@gmail.com>
2015-09-04 10:31:32 -0400

    add more

---
+++ from_county/nightly/bc_location_table-to-warehouse.bat
@@ -0,0 +1,1 @@
+fme "C:\Users\mssqlgisadmin\Documents\GitHub\fme-jobs\from_county\nightly\bc_location_table-to-warehouse.tcl"
---
+++ from_county/nightly/bc_property-to-edit.bat
@@ -0,0 +1,1 @@
+fme "C:\Users\mssqlgisadmin\Documents\GitHub\fme-jobs\from_county\nightly\bc_property-to-edit.tcl"
